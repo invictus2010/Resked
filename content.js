ddab7c03d0f0fa00483e3ee436b6bf89b4eaaceb
alexjbuck <abuck@spear.ai>
2024-01-14 08:57:03 -0500

    Refactor for early return pattern
    Prefer the early return pattern to nested If's.

--- content.js
+++ content.js
@@ -4,36 +4,40 @@
 function checkForIframe() {
   const iframeElement = document.querySelector('iframe'); 
 
-  if (iframeElement) {
-    const iframeContentDocument = iframeElement.contentDocument;
-
-    const inputs = get_input_elements(iframeContentDocument);
-    if (inputs.every((input) => input !== null)) {
-      const [inputUsername, inputPhone, inputEmail, inputPhoneType] = inputs;
-      chrome.storage.sync.get(['user'], function(result) {
-        const user = JSON.parse(result.user);
-        if (user) {
-          inputUsername.value = user.username;
-          inputPhone.value = user.phone;
-          inputEmail.value = user.email;
-          inputPhoneType.value = "D";
-        }
-      });
-    } else {
-      // If input element is not found, retry after a delay
-      console.log('Input element not found, retrying...');
-      setTimeout(checkForIframe, 2000); // Wait for 2 seconds and then retry
-    }
-  } else {
-    // If iframe is not found, retry after a delay
-    console.log('Iframe not found, retrying...');
+  if (iframeElement === null) {
     setTimeout(checkForIframe, 2000); // Wait for 2 seconds and then retry
+    return;
   }
+
+  const inputs = get_input_elements(iframeElement.contentDocument);
+  const allInputsFound = inputs.every((input) => input !== null);
+  if (!allInputsFound) {
+    console.log('Input element not found, retrying...');
+    setTimeout(checkForIframe, 2000); // Wait for 2 seconds and then retry
+    return;
+  }
+  const [inputUsername, inputPhone, inputEmail, inputPhoneType] = inputs;
+
+  chrome.storage.sync.get(['user'])
+  .then((result) => {
+    const user = JSON.parse(result.user);
+    if (user) {
+      inputUsername.value = user.username;
+      inputPhone.value = user.phone;
+      inputEmail.value = user.email;
+      inputPhoneType.value = "D";
+    }
+  });
 }
 
 // Call the function to start checking for the iframe
 setTimeout(checkForIframe, 5000);
 
+/**
+ * Retrieves input elements from the provided iframe content document.
+ * @param {Document} iframeContentDocument - The document object of the iframe content.
+ * @returns {Array} - An array containing the input elements.
+ */
 function get_input_elements(iframeContentDocument) {
     const inputUsername = iframeContentDocument.getElementById('F_EDM_IDT_RQST_NAME');
     const inputPhone = iframeContentDocument.getElementById('F_EDM_IDT_RQST_PHONE');
